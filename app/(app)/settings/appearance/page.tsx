58f948be29bbc14c1f03879f4ed116bcae6f12f7
itsvocs <jordanpouani@icloud.com>
2026-04-25 15:51:37 +0200

    set dark theme

--- app/(app)/settings/appearance/page.tsx
+++ app/(app)/settings/appearance/page.tsx
@@ -1,64 +1,65 @@
-import { CheckIcon, MinusIcon } from "lucide-react";
+"use client";
 
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Radio, RadioGroup } from "@/components/ui/radio-group";
+import Image from "next/image";
+import { Label } from "@/components/ui/label";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 const items = [
-  { image: "https://coss.com/origin/ui-light.png", label: "Light", value: "1" },
-  { image: "https://coss.com/origin/ui-dark.png", label: "Dark", value: "2" },
+  { image: "/ui-light.png", label: "Light", value: "light" },
+  { image: "/ui-dark.png", label: "Dark", value: "dark" },
   {
-    image: "https://coss.com/origin/ui-system.png",
+    image: "/ui-system.png",
     label: "System",
-    value: "3",
+    value: "system",
   },
 ];
 
 export default function Page() {
+  const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  if (!theme) return null;
   return (
     <div className="sm:p-10 p-4">
       <h1 className="text-2xl font-bold tracking-tight">Erscheinungsbild</h1>
       <p className="text-muted-foreground">
-        Passe das Erscheinungsbild von Routine an deine Vorlieben an
+        Passe das Erscheinungsbild der Platform an deine Vorlieben an
       </p>
 
-      <div className="mt-8">
-        <fieldset className="space-y-4">
-          <legend className="font-medium text-foreground text-sm leading-none">
-            Wähle ein Thema
-          </legend>
-          <RadioGroup className="flex gap-3 flex-row" defaultValue="1">
-            {items.map((item) => (
-              <label key={`label-${item.value}`}>
-                <RadioGroupItem
-                  className="peer sr-only after:absolute after:inset-0"
-                  id={`label-${item.value}`}
-                  value={item.value}
-                />
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  alt={item.label}
-                  className="relative cursor-pointer overflow-hidden rounded-md border border-input shadow-xs outline-none transition-[color,box-shadow] peer-focus-visible:ring-[3px] peer-focus-visible:ring-ring/50 peer-data-disabled:cursor-not-allowed peer-data-[state=checked]:border-ring peer-data-[state=checked]:bg-accent peer-data-disabled:opacity-50"
-                  height={70}
-                  src={item.image}
-                  width={88}
-                />
-                <span className="group mt-2 flex items-center gap-1 peer-data-[state=unchecked]:text-muted-foreground/70">
-                  <CheckIcon
-                    aria-hidden="true"
-                    className="group-peer-data-[state=unchecked]:hidden"
-                    size={16}
-                  />
-                  <MinusIcon
-                    aria-hidden="true"
-                    className="group-peer-data-[state=checked]:hidden"
-                    size={16}
-                  />
-                  <span className="font-medium text-xs">{item.label}</span>
-                </span>
-              </label>
-            ))}
-          </RadioGroup>
-        </fieldset>
-      </div>
+      <RadioGroup
+        value={theme}
+        className="mt-8 flex  flex-row"
+        onValueChange={(value) => setTheme(value)}>
+        {items.map((item) => (
+          <div key={item.value} className="flex flex-col items-center gap-2">
+            <Label className="flex items-start gap-2 rounded-lg border p-3 hover:bg-accent/50 has-data-checked:border-primary/48 has-data-checked:bg-accent/50">
+              <Radio
+                value={item.value}
+                className="peer sr-only after:absolute after:inset-0 hidden"
+              />
+              <Image
+                src={item.image}
+                alt={`${item.label} theme preview`}
+                width={88}
+                height={70}
+                className="relative cursor-pointer overflow-hidden rounded-md border border-input shadow-xs outline-none"
+              />
+            </Label>
+            <div className="flex flex-row gap-6 items-center justify-center">
+              <p>{item.label}</p>
+            </div>
+          </div>
+        ))}
+      </RadioGroup>
     </div>
   );
 }
